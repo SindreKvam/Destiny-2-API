fe0f0afeb0e32c6ce64a914cc4bc2b3dcd504cd9
SindreKvam <s.m.k.1@hotmail.com>
2020-12-14 21:34:08 +0100

    Added HTML, getting somewhere.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import requests
 from requests_oauthlib import OAuth2Session
 from definitions import printjson
+from flask import Flask, render_template, request, redirect
 import json
 
 # http://destinydevs.github.io/BungieNetPlatform/docs/Getting-Started
@@ -11,11 +12,15 @@ import json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
+app = Flask(__name__)
+
+# Collecting inputs from the config file
 API_KEY = config['APIkey']
 HEADERS = {"X-API-Key": f"{API_KEY}"}
 CLIENT_ID = config["ClientId"]
 CLAN_ID = config['ClanId']
 
+# Start link for almost all requests
 ENDPOINT = "https://www.bungie.net/Platform"
 
 redirect_uri = 'https://destinygetauth-key.no/'
@@ -27,27 +32,54 @@ authorization_url, state = oauth.authorization_url(
         )
 
 
-print(f'Please go to {authorization_url} and authorize access.')
-authorization_response = input('Enter the full callback URL ')
+# print(f'Please go to {authorization_url} and authorize access.')
+# authorization_response = input('Enter the full callback URL ')
 
-token = oauth.fetch_token(
+# token = oauth.fetch_token(
+#        'https://www.bungie.net/platform/app/oauth/token',
+#        authorization_response=authorization_response,
+#        include_client_id=True,
+#        # authentication
+#        )
+# access_token = token['access_token']
+
+@app.route('/login')
+def login():
+    print(f'Please go to {authorization_url} and authorize access.')
+
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    authorization_response = request.form['login']
+    token = oauth.fetch_token(
         'https://www.bungie.net/platform/app/oauth/token',
         authorization_response=authorization_response,
         include_client_id=True,
         # authentication
-        )
-access_token = token['access_token']
+    )
+    access_token = token['access_token']
+    return redirect('/info')
 
 
-print("Signed in information")
-signedIn = oauth.get(   # oauth.get instead of request.get because auth code is saved in oauth library.
-    f'{ENDPOINT}/User/GetMembershipsForCurrentUser/',
-    headers=HEADERS
-)
-printjson(signedIn)
+@app.route('/')
+def index():
+    return 'Hello world'
 
-MEMBERSHIP_TYPE = signedIn.json()['Response']['destinyMemberships'][0]['membershipType']
-USER_ID = signedIn.json()['Response']['bungieNetUser']['membershipId']
+
+@app.route('/info')
+def info():
+    print("Signed in information")
+    signedIn = oauth.get(   # oauth.get instead of request.get because auth code is saved in oauth library.
+        f'{ENDPOINT}/User/GetMembershipsForCurrentUser/',
+        headers=HEADERS
+    )
+    printjson(signedIn)
+
+    MEMBERSHIP_TYPE = signedIn.json()['Response']['destinyMemberships'][0]['membershipType']
+    USER_ID = signedIn.json()['Response']['bungieNetUser']['membershipId']
+    return redirect('/')
 
 
 def clanstatus():
@@ -70,3 +102,5 @@ def pullfrompostmaster():
     printjson(pullPostmaster)
 
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
